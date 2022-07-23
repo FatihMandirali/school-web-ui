30e0716958d13efcffeb75b202db3581bae07bf0
fm <fatih.mandirali@hotmail.com>
2022-07-23 13:00:47 +0300

    Revert "table folder remove"
    This reverts commit 3daa1547807dbaaae4f007f6e36e39046d7a90af.

---
+++ school-web-ui/school-web-ui/src/layouts/tables/index.js
@@ -0,0 +1,124 @@
+/**
+ =========================================================
+ * Material Dashboard 2 React - v2.1.0
+ =========================================================
+
+ * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+ * Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+ Coded by www.creative-tim.com
+
+ =========================================================
+
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ */
+
+// @mui material components
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import Footer from "examples/Footer";
+
+import { DataGrid, GridActionsCellItem } from "@mui/x-data-grid";
+import { useEffect, useState } from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import Icon from "@mui/material/Icon";
+import { Link } from "react-router-dom";
+import useList from "./service/useList";
+import MDButton from "../../components/MDButton";
+import MDTypography from "../../components/MDTypography";
+
+function Tables() {
+  const [email, setEmail] = useState(0);
+  const { service, get } = useList(email);
+
+  const handleEditClick = (id) => () => {
+    window.location.href = `/dashboard/${id}`;
+  };
+
+  const columns = [
+    { field: "AdminName", headerName: "Admin Adı", width: 200 },
+    { field: "AdminSurname", headerName: "Admin Soyadı", minWidth: 400 },
+    {
+      field: "RoleName",
+      headerName: "Admin Rolü",
+      minWidth: 200,
+      // valueGetter: (params) => (params.row.adminRole === 1 ? "Admin" : "Değil"),
+    },
+    {
+      field: "username",
+      headerName: "Kullanıcı Adı",
+      minWidth: 200,
+    },
+    {
+      field: "BranchName",
+      headerName: "Bölgesi",
+      minWidth: 200,
+    },
+    {
+      field: "actions",
+      type: "actions",
+      headerName: "Detay",
+      width: 100,
+      cellClassName: "actions",
+      // eslint-disable-next-line react/no-unstable-nested-components
+      getActions: ({ id }) => [
+        <GridActionsCellItem
+          icon={<EditIcon />}
+          label="Edit"
+          className="textPrimary"
+          onClick={handleEditClick(id)}
+          color="inherit"
+        />,
+      ],
+    },
+  ];
+
+  const changePage = (page) => {
+    console.log("page");
+    console.log(page);
+    setEmail(page);
+    useEffect(() => {
+      get(email);
+    }, [email]);
+  };
+
+  return (
+    <DashboardLayout>
+      <MDBox pt={6} pb={3}>
+        <MDBox pt={2} px={2} display="flex" justifyContent="space-between" alignItems="center">
+          <MDTypography variant="h6" fontWeight="medium">
+            <h1>Kullanıcılar</h1>
+          </MDTypography>
+          <Link to="/dashboard" style={{ color: "#FFF" }}>
+            <MDButton variant="gradient" color="dark">
+              <Icon sx={{ fontWeight: "bold" }}>add</Icon>
+              &nbsp;ekle
+            </MDButton>
+          </Link>
+        </MDBox>
+        <br />
+        {service.serviceStatus === "loaded" && (
+          <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={service.data}
+              columns={columns}
+              pageSize={10}
+              pagination
+              getRowId={(row) => row.AdminId}
+              rowsPerPageOptions={[5, 10, 15]}
+              onPageChange={(newPage) => changePage(newPage)}
+              loading={service.serviceStatus === "loading"}
+            />
+          </div>
+        )}
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Tables;
