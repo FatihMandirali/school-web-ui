8845f670822ad5424a4e160afb23021ad21a7698
fm <fatih.mandirali@hotmail.com>
2022-09-27 15:49:24 +0300

    admin create formik control edited

--- school-web-ui/school-web-ui/src/layouts/users/validations/userValidation.js
+++ school-web-ui/school-web-ui/src/layouts/users/validations/userValidation.js
@@ -5,7 +5,7 @@ export const validationSchema = Yup.object({
   firstName: Yup.string().required("Lütfen adını girin."),
   surname: Yup.string().required("Lütfen soyadını girin."),
   userName: Yup.string().required("Lütfen kullanıcı adını girin."),
-  tc: Yup.number().typeError("Lütfen rakam girin").required("Lütfen tc girin."),
+  tc: Yup.string().required("Lütfen tc girin."),
   branchId: Yup.number().min(1, "Lütfen bölge seçin"),
   pass: Yup.string().required("Lütfen şifre girin."),
 });
