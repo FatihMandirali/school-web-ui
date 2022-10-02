8b627514f2a2396eb3b5bcd9bd241932c70c0e7d
fm <fatih.mandirali@hotmail.com>
2022-10-02 12:06:28 +0300

    firstpayment edited

--- school-web-ui/school-web-ui/src/layouts/students/validations/studentPaymentValidation.js
+++ school-web-ui/school-web-ui/src/layouts/students/validations/studentPaymentValidation.js
@@ -3,7 +3,5 @@ import * as Yup from "yup";
 // eslint-disable-next-line import/prefer-default-export
 export const validationSchema = Yup.object({
   totalAmount: Yup.number().min(1, "Lütfen toplam miktarı seçin"),
-  firstPaymentDate: Yup.date()
-    .required("Lütfen ilk ödeme tarihini seçiniz.")
-    .min(new Date(), "Lütfen bugünden önceki bir tarihi seçmeyiniz."),
+  firstPaymentDate: Yup.date().required("Lütfen ilk ödeme tarihini seçiniz."),
 });
